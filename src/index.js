aa4f001d86a4ddf66198b6b05132c20914c1be0f
Yetsing <yetsingzhu@126.com>
2018-07-17 15:46:43 +0800

    热更新

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+
+function component() {
+    var element = document.createElement('div');
+    element.innerHTML = 'Hello Webpack!';
+    element.classList.add('hello');
+    return element;
+}
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
